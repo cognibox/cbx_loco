fc400d31695167e7c8dd5e58e83ab9dfe5f8a20a
David Lauzon <david.lauzon@alcumus.com>
2024-01-31 13:21:09 -0500

    PLAT-308 simpler Net::HTTP calls and tests

--- lib/cbx_loco/adapter.rb
+++ lib/cbx_loco/adapter.rb
@@ -7,29 +7,13 @@ class CbxLoco::Adapter
     params = params.merge(key: CbxLoco.configuration.api_key)
     params = params.merge(v: CbxLoco.configuration.version) if CbxLoco.configuration.version
 
-    uri = URI.parse(CbxLoco.configuration.api_url + api_path)
-    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get(URI(CbxLoco.configuration.api_url + api_path + "?#{URI.encode_www_form(params)}"))
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == 'https')
-
-    request = Net::HTTP::Get.new(uri)
-    res = http.request(request)
-
-    json ? JSON.parse(res.body) : res.body
+    json ? JSON.parse(res) : res
   end
 
   def self.post(api_path, params = {})
-    uri = URI.parse(CbxLoco.configuration.api_url + api_path)
-    uri.query = "key=#{CbxLoco.configuration.api_key}"
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == 'https')
-
-    request = Net::HTTP::Post.new(uri)
-    request.set_form_data(params)
-
-    res = http.request(request)
+    res = Net::HTTP.post(URI(CbxLoco.configuration.api_url + api_path + "?key=#{CbxLoco.configuration.api_key}"), URI.encode_www_form(params))
 
     JSON.parse res.body
   end
--- spec/cbx_loco/adapter_spec.rb
+++ spec/cbx_loco/adapter_spec.rb
@@ -1,7 +1,4 @@
 require "spec_helper"
-require "json"
-require "net/http"
-
 
 describe CbxLoco::Adapter do
   let(:str_response) { rand_str }
@@ -18,73 +15,66 @@ describe CbxLoco::Adapter do
 
   describe "get" do
     before(:each) do
-      http_double = instance_double(Net::HTTP)
-      allow(http_double).to receive(:use_ssl=)
-      allow(Net::HTTP).to receive(:new).and_return(http_double)
-      response_double = instance_double(Net::HTTPResponse, body: str_json)
-      allow(http_double).to receive(:request).and_return(response_double)
+      allow(Net::HTTP).to receive(:get).and_return(str_json)
     end
 
-    it "sends a GET request and parses the JSON response by default" do
-      result = CbxLoco::Adapter.get("test")
-
-      expect(result).to eq("test" => str_response)
-    end
-
-    it "sends a GET request and returns the raw response body if json is false" do
-      get_params = {  key1: "value1", key2: "value2" }
-      result = CbxLoco::Adapter.get("test", get_params, false)
-
-      expect(result).to eq(str_json)
+    it "should call Net::HTTP.get" do
+      CbxLoco::Adapter.get("test")
+      expect(Net::HTTP).to have_received(:get)
     end
 
     it "should build the request URL" do
       random_str = rand_str
-      result = CbxLoco::Adapter.get(random_str)
-      expect(result).to eq "test" => str_response
+      CbxLoco::Adapter.get(random_str)
+      expect(Net::HTTP).to have_received(:get) do |uri|
+        expect(uri.to_s).to start_with("#{fake_api_url}#{random_str}")
+      end
     end
 
     context "when api version" do
       it "should send it to get" do
         CbxLoco.configuration.version = "1.0.0"
-        get_params = { key: fake_api_key, v: "1.0.0" }
-        allow(URI).to receive(:encode_www_form)
-        result = CbxLoco::Adapter.get("test")
-
-        expect(URI).to have_received(:encode_www_form).with(get_params)
+        CbxLoco::Adapter.get("test")
+        get_params = { key: fake_api_key }
+        get_params[:v] = CbxLoco.configuration.version
+        expect(Net::HTTP).to have_received(:get) do |uri|
+          expect(uri.to_s).to include("key=#{fake_api_key}").and(include("v=1.0.0"))
+        end
       end
     end
 
     context "when no api version" do
       it "should not send version" do
         CbxLoco.configuration.version = nil
+        CbxLoco::Adapter.get("test")
         get_params = { key: fake_api_key }
-        allow(URI).to receive(:encode_www_form)
-        result = CbxLoco::Adapter.get("test")
-
-        expect(URI).to have_received(:encode_www_form).with(get_params)
+        expect(Net::HTTP).to have_received(:get) do |uri|
+          expect(uri.to_s).to include("key=#{fake_api_key}")
+          expect(uri.to_s).not_to include("v=")
+        end
       end
     end
 
     it "should build the request parameters" do
       random_sym = rand_str.to_sym
       random_str = rand_str
-      allow(URI).to receive(:encode_www_form)
-      result = CbxLoco::Adapter.get("test", random_sym => random_str)
+      CbxLoco::Adapter.get("test", random_sym => random_str)
       get_params = { key: fake_api_key, random_sym => random_str }
       get_params[:v] = CbxLoco.configuration.version if CbxLoco.configuration.version.present?
-
-      expect(URI).to have_received(:encode_www_form).with(get_params)
+      expect(Net::HTTP).to have_received(:get) do |uri|
+        expect(uri.to_s).to include("key=#{fake_api_key}").and(include("#{random_sym}=#{random_str}"))
+        expect(uri.to_s).to include("v=#{CbxLoco.configuration.version}") if CbxLoco.configuration.version.present?
+      end
     end
 
     it "should prevent overriding the API key" do
       random_str = rand_str
-      allow(URI).to receive(:encode_www_form)
       CbxLoco::Adapter.get("test", key: random_str)
       get_params = { key: fake_api_key }
       get_params[:v] = CbxLoco.configuration.version if CbxLoco.configuration.version.present?
-
-      expect(URI).to have_received(:encode_www_form).with(get_params)
+      expect(Net::HTTP).to have_received(:get) do |uri|
+        expect(uri.to_s).to include("key=#{fake_api_key}")
+      end
     end
 
     context "with json undefined or true" do
@@ -99,46 +89,33 @@ describe CbxLoco::Adapter do
         expect(CbxLoco::Adapter.get("test", {}, false)).to eq str_json
       end
     end
-    it 'sends the correct request and parses the response' do
-      expected_url = "#{fake_api_url}test"
-      response_body = '{"key": "value"}'
-
-      uri = URI.parse(expected_url)
-
-      http_double = double("http", request: double("request", body: response_body))
-      allow(Net::HTTP).to receive(:new).and_return(http_double)
-
-      expect(http_double).to receive(:request).with(an_instance_of(Net::HTTP::Get)) do |request_arg|
-        expect(request_arg.path).to eq(uri.path)
-        expect(request_arg.body).to eq(URI.encode_www_form(params))
-      end
-    end
   end
 
   describe "post" do
     before(:each) do
-      http_double = instance_double(Net::HTTP, request: double(body: '{"result": "success"}'))
-      allow(http_double).to receive(:use_ssl=)
-      allow(Net::HTTP).to receive(:new).and_return(http_double)
+      allow(Net::HTTP).to receive(:post).and_return(double(body: str_json))
     end
 
-    it "sends a POST request and parses the JSON response" do
-      result = CbxLoco::Adapter.post("test")
+    it "should call RestClient.post" do
+      CbxLoco::Adapter.post("test")
+      expect(Net::HTTP).to have_received(:post)
+    end
 
-      expect(Net::HTTP).to have_received(:new).with("example.com", 80)
-      expect(result).to eq("result" => "success")
+    it "should build the request URL" do
+      random_str = rand_str
+      CbxLoco::Adapter.post(random_str)
+      expect(Net::HTTP).to have_received(:post) do |uri, _data|
+        expect(uri.to_s).to eq("#{fake_api_url}#{random_str}?key=#{fake_api_key}")
+      end
     end
 
     it "should use the untouched request parameters" do
-      request_double = instance_double(Net::HTTP::Post, set_form_data: nil)
-      allow(Net::HTTP::Post).to receive(:new).and_return(request_double)
-
       random_sym = rand_str.to_sym
       random_str = rand_str
-
-      CbxLoco::Adapter.post("test",  random_sym => random_str)
-
-      expect(request_double).to have_received(:set_form_data).with(random_sym => random_str)
+      CbxLoco::Adapter.post("test", random_sym => random_str)
+      expect(Net::HTTP).to have_received(:post) do |_uri, data|
+        expect(data).to eq("#{random_sym}=#{random_str}")
+      end
     end
   end
 end
